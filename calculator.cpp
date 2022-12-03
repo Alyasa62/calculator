a9c58de23f54e1922925a9ffc47c0ab40650ed2d
Muhammad Al Yasa <m.alyasa62@gmail.com>
2022-12-03 22:52:03 +0500

    #1

--- calculator.cpp
+++ calculator.cpp
@@ -3,27 +3,45 @@
 using namespace std;
 
 int main() {
-    float num1, num2;
-    int choice;
+	float num1, num2;
+	int choice;
+	
+	while (true) {
+		// Get input from user.
+    	cout << "Enter first number: ";  
+	    cin >> num1;
+	    cout << "Enter second number: ";
+	    cin >> num2;
 
-    cout << "Enter first number: ";
-    cin >> num1;
+		// Print the menu.
+	    cout << " --- [Calculator] --- " << endl; 
+	    cout << "1. Addition." << endl;
+	    cout << "2. Subtraction." << endl;
+	    cout << "3. Multiplication." << endl;
+	    cout << "4. Division." << endl;
+	    cout << "5. Exit." << endl;
 
-    cout << "Enter second number: ";
-    cin >>  num2;
+		// Get user's choice.
+	    cout << "Please enter your choice: ";
+	    cin >> choice;
 
-    cout << "1. Addition." << endl;
-    cout << "2. Subtraction." << endl;
-
-    cout << "Enter your choice: ";
-    cin >> choice;
-
-    if (choice == 1) {
-        cout << "Sum is: " << num1 + num2;
-    }
-    else if (choice == 2) {
-        cout << "Differance is: " << num1 - num2;
-    }
-
-    return 0;
+		// Perform operations based on choice.
+	    if (choice == 1) {
+	      cout << "Sum of " << num1 << " and " << num2 << " is: " << num1 + num2 << endl;
+	    }
+		else if (choice == 2) {
+	      cout << "Difference of " << num1 << " and " << num2 << " is: " << num1 - num2 << endl;
+		}
+		else if (choice == 3) {
+	      cout << "Product of " << num1 << " and " << num2 << " is: " << num1 * num2 << endl;
+		}
+		else if (choice == 4 ) {
+	      cout << "Division of " << num1 << " and " << num2 << " is: " << num1 / num2 << endl;
+		}
+		else if (choice == 5) {
+            
+			break;
+		}
+		cout << "\n";
+	}
 }
